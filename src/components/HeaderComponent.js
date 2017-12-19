3859882ad91c86a07a5ef8562c9b84cbef207097
Hemant Sankhla <hemantsan@cybage.com>
2017-12-19 14:51:23 +0530

    Active link implemented and css file added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,30 +1,31 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import SiteCSS from '../Styles/site.css';
 
 class HeaderComponent extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-md navbar-light bg-light mb-4">
-                <Link className="navbar-brand" to={"/"}>React Router</Link>
+                <NavLink className="navbar-brand" to={"/"}>React Router</NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <Link className="nav-link" to={"/home"}>Home <span className="sr-only">(current)</span></Link>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to={"/home"}>Home <span className="sr-only">(current)</span></NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to={"/profile/view"}>Profile</Link>
+                            <NavLink className="nav-link" to={"/profile"}>Profile</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to={"/explore/featured"}>Explore</Link>
+                            <NavLink className="nav-link" to={"/explore/featured"}>Explore</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to={"/wishlist/offers"}>Wishlist</Link>
+                            <NavLink className="nav-link" to={"/wishlist/offers"}>Wishlist</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to={"/games/2018/latest"}>Latest</Link>
+                            <NavLink className="nav-link" to={"/games/2018/latest"}>Latest</NavLink>
                         </li>
                     </ul>
                 </div>
